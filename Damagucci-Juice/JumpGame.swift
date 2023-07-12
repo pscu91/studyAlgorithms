e0fda5c800c5e77f54ead1dde4de9bc2d586a61b
Damagucci-Juice <jsr9874@gmail.com>
2023-07-12 17:07:15 +0900

    [LTC] Jump Game 2 / Medium / 1h 48m / Swift
    https://leetcode.com/problems/jump-game-ii/?envType=study-plan-v2&envId=top-interview-150

--- Damagucci-Juice/JumpGame.swift
+++ Damagucci-Juice/JumpGame.swift
@@ -1,15 +1,10 @@
 class Solution {
-    func canJump(_ nums: [Int]) -> Bool {
-        guard nums.count > 1 else { return true }
-        guard nums[0] != 0 else { return false }
-        
-        var availables: [Int] = [nums[0]]
-        for i in 1..<nums.count-1 {
-            availables.append(max(availables[i-1]-1, nums[i]))
-            if availables[i] == 0 {
-                return false 
-            }
+    func jump(_ nums: [Int]) -> Int {
+        var step = 0, end = 0, maxPos = 0 
+        for i in 0..<nums.count-1 {
+            maxPos = max(maxPos, i+nums[i])
+            if end == i { end = maxPos; step += 1 }
         }
-        return true
+        return step
     }
 }
